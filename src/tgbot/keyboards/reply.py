f3e4555e47bb11dc1695e4535c6251fbbc7b7929
Ruslan <kornerus@fjfi.cvut.cz>
2022-05-29 15:54:38 +0300

    refactoring

--- src/tgbot/keyboards/reply.py
+++ src/tgbot/keyboards/reply.py
@@ -4,13 +4,7 @@ from settings.text import BUTTONS_TEXT
 
 
 def menu_keyboard() -> ReplyKeyboardMarkup:
-    """
-    Return a menu keyboard with this buttons:
-
-    | get info | profile |
-    |--------------------|
-    |     get contacts   |
-    """
+    """ Return a menu keyboard with this buttons """
     keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
     keyboard.add(
         KeyboardButton(BUTTONS_TEXT['GET_INFO']),
