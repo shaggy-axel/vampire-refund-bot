ad1d3aa5a9172ee307542f00492d4963f773437e
Ruslan <kornerus@fjfi.cvut.cz>
2022-06-17 01:27:29 +0300

    change button places for admin user

--- src/tgbot/keyboards/reply.py
+++ src/tgbot/keyboards/reply.py
@@ -6,15 +6,18 @@ from settings.text import BUTTONS_TEXT
 def menu_keyboard(is_admin: bool = False) -> ReplyKeyboardMarkup:
     """ Return a menu keyboard with this buttons """
     keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-    keyboard.add(
-        KeyboardButton(BUTTONS_TEXT['GET_INFO']),
-        KeyboardButton(BUTTONS_TEXT['PROFILE']),
-    )
-    keyboard.add(KeyboardButton(BUTTONS_TEXT["CONTACTS"]))
 
     if is_admin:
         keyboard.add(
             KeyboardButton(BUTTONS_TEXT['ORDERS']),
             KeyboardButton(BUTTONS_TEXT['CREATE_ADDRESS']),
         )
+    else:
+        keyboard.add(KeyboardButton(BUTTONS_TEXT["CONTACTS"]))
+
+    keyboard.add(
+        KeyboardButton(BUTTONS_TEXT['GET_INFO']),
+        KeyboardButton(BUTTONS_TEXT['PROFILE']),
+    )
+
     return keyboard
